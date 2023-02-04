91946caeeef23591d91d9417dce701ae55801ac9
Alexander <agmikhaylenko@gmail.com>
2023-02-04 03:07:30 +0300

    init commit

---
+++ src/easy/tree/SymmetricTree.java
@@ -0,0 +1,44 @@
+package easy.tree;
+
+/**
+ * Easy
+ * https://leetcode.com/problems/symmetric-tree
+ */
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+
+        return left.val == right.val
+                && isSymmetric(left.left, right.right)
+                && isSymmetric(left.right, right.left);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
